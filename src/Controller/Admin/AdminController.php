470705a79dbfdc4e7c16350840e07e35bc01ba8a
babacar ndiaye <babacar.ndiaye20@uvs.edu.sn>
2022-03-11 09:28:05 +0000

    ajout du template

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return $this->render('admin/home.html.twig');
+    }
+}
