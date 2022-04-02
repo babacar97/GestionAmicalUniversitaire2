2f1021a12364edff4d6a400aa1dc9a601322c484
babacar ndiaye <babacar.ndiaye20@uvs.edu.sn>
2022-04-02 12:45:45 +0000

    initialisation du comptabliteController

---
+++ src/Controller/Comptablite/ComptablityController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Comptablite;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ComptablityController extends AbstractController
+{
+    /**
+     * @Route("/comptablity", name="app_comptablity")
+     */
+    public function index(): Response
+    {
+        return $this->render('comptablity/index.html.twig', [
+            'controller_name' => 'ComptablityController',
+        ]);
+    }
+}
